bab0ea761a509a97c690c473c10e72732b40449f
artempelevin <artempelevin@list.ru>
2024-02-04 15:11:42 +0300

    refactor: уменьшено кол-во строк в `UniversityManager`

--- src/managers/university.py
+++ src/managers/university.py
@@ -11,12 +11,10 @@ class UniversityManager:
     async def get_all() -> Sequence[University]:
         async with Session() as session:
             query = select(University)
-            univ_stmt = await session.execute(query)
-            return univ_stmt.scalars().all()
+            return (await session.execute(query)).scalars().all()
 
     @staticmethod
     async def get_by_id(university_id: int) -> University | None:
         async with Session() as session:
             query = select(University).where(University.id == university_id)
-            universities = await session.execute(query)
-            return universities.scalar_one_or_none()
+            return (await session.execute(query)).scalar_one_or_none()
