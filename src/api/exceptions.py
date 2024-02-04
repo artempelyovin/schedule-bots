e52fb3420153b8758934875b4fc6d09cfbac342d
artempelevin <artempelevin@list.ru>
2024-02-04 13:46:05 +0300

    refactor: Удалено упоминание `pylint`

--- src/api/exceptions.py
+++ src/api/exceptions.py
@@ -4,10 +4,7 @@ from starlette.responses import JSONResponse
 from src.api.utils import Response
 
 
-async def http_exception_handler(
-    request: Request,
-    exc: HTTPException,  # pylint: disable=unused-argument
-) -> JSONResponse:
+async def http_exception_handler(request: Request, exc: HTTPException) -> JSONResponse:
     return JSONResponse(
         status_code=exc.status_code,
         content=Response(data=None, status_code=exc.status_code, error=True, detail=exc.detail).model_dump(),
