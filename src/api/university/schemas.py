e7aa5fd6edeccb58ae56469f6c090493805bf072
artempelevin <artempelevin@list.ru>
2024-02-04 15:20:20 +0300

    feat: Добавлены описания для `UniversityScheme`

--- src/api/university/schemas.py
+++ src/api/university/schemas.py
@@ -1,10 +1,12 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 
 
 class UniversityScheme(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
-    id: int
-    short_name: str
-    full_name: str
-    description: str
+    id: int = Field(description="ID университета")
+    short_name: str = Field(description="Аббревиатура университета", examples=["МГУ"])
+    full_name: str = Field(
+        description="Расшифровка аббревиатуры университета", examples=["Московский государственный университет"]
+    )
+    description: str = Field(description="Короткое описание института", examples=["Один из главных престижных России"])
