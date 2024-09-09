03d0f0017559e3eb9f5567c169bee30bbd8d6b15
artempelevin <artempelevin@list.ru>
2024-09-09 21:42:24 +0300

    refactor: fix commented-out-code (`ERA001`)
    https://docs.astral.sh/ruff/rules/commented-out-code/

--- src/fe/vk/handlers/error_report.py
+++ src/fe/vk/handlers/error_report.py
@@ -18,13 +18,3 @@ dialog = ErrorReportDialog()
 @save_user
 async def error_report_handler(event: MessageEvent):
     await event.edit_message(dialog.message, keyboard=generate_vk_keyboard(dialog.keyboard))
-
-
-# @labeler.message(state=GlobalState.ERROR_REPORT)
-# async def thanks_for_error_report_handler(message: Message):
-#     await message.answer(THANKS_FOR_ERROR_REPORT)
-#     payload = await state_dispenser.get(message.peer_id).payload
-#     prevision_state = payload["prevision_state"]
-#     prevision_state = prevision_state.split(":")[-1]  # format: ClassName:value
-#     prevision_state = GlobalState(prevision_state)
-#     await state_dispenser.set(message.peer_id, prevision_state)
